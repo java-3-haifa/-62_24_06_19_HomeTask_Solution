ba443aba556a2400727cf47970ba207345fb543d
Gregory Sheygam <sheygam@gmail.com>
2019-06-17 08:40:21 +0300

    init project

---
+++ src/main/java/com/telran/hometask61solution/Hometask61SolutionApplication.java
@@ -0,0 +1,13 @@
+package com.telran.hometask61solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometask61SolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hometask61SolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telran/hometask61solution/Hometask61SolutionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telran.hometask61solution;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hometask61SolutionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
